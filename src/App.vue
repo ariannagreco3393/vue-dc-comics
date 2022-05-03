0d1a965c1a15054a8341c2f60fc1db7284ac7a1a
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-03 15:16:04 +0200

    add header compononet

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,7 @@
     <link
       href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
       rel="stylesheet"/>
-    <header id="site_header">
-      <div class="container">
-        <div class="row">
-          <div class="col-4">
-            <img src="@/assets/img/dc-logo.png" alt="">
-          </div>
-          <div class="col-8">
-            <nav>
-              <a href="">CHARACTERS</a>
-              <a href="" class="active">COMICS</a>
-              <a href="">MOVIES</a>
-              <a href="">TV</a>
-              <a href="">GAMES</a>
-              <a href="">COLLECTIBLES</a>
-              <a href="">VIDEOS</a>
-              <a href="">FANS</a>
-              <a href="">NEWS</a>
-              <a href="">SHOP</a>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </header>
+    <siteHeader/>
     <!-- /#site_header -->
     <main id="site_main"></main>
     <!-- /#site_main -->
@@ -37,9 +15,13 @@
 </template>
 
 <script>
+import siteHeader from './components/headerComponent.vue'
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    siteHeader
+  },
 };
 </script>
 
@@ -47,48 +29,20 @@ export default {
 #app {
   font-family: 'Lato', sans-serif;
 }
+
+/* common rules */
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-
-#site_header {
-  height: 100px;
-  margin: 1rem 0;
-
-  img {
-    height: 80px;
-  }
-
-  a {
-    text-decoration: none;
-    padding: 2.5rem 0rem;
-    font-size: 14px;
-    font-weight: 700;
-    color: #464646;
-    margin: 0 0.85rem;
-  }
-
-  nav {
-
-    .active {
-      color: rgb(0,130,249);
-      border-bottom: 4px solid rgb(0,130,249);
-    }
-  }
-  .col-8 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-}
-
-/* common rules */
 img {
   max-width: 100%;
 }
+
+/* importazioni */
 
 @import "@/assets/sass/_grid_system.scss";
 </style>
---
+++ src/components/headerComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <header id="site_header">
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <img src="@/assets/img/dc-logo.png" alt="" />
+        </div>
+        <div class="col-8">
+          <nav>
+            <a href="#" v-for="(voce, index) in mainMenu" :key="index">{{voce}}</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+
+<script>
+export default {
+    name: 'headerComponent',
+    data() {
+        return {
+            mainMenu: ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 
+            'VIDEOS', 'FANS', 'NEWS', 'SHOP']
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+#site_header {
+  height: 100px;
+  margin: 1rem 0;
+
+  img {
+    height: 80px;
+  }
+
+  a {
+    text-decoration: none;
+    padding: 2.5rem 0rem;
+    font-size: 14px;
+    font-weight: 700;
+    color: #464646;
+    margin: 0 0.85rem;
+  }
+
+  nav {
+    .active {
+      color: rgb(0, 130, 249);
+      border-bottom: 4px solid rgb(0, 130, 249);
+    }
+  }
+  .col-8 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
